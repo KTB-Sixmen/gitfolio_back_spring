8d6fbfdf2d76dc4c22e7b96a6175f7132e096a11
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-10-08 22:34:17 +0900

    feat: auth 모듈 생성

---
+++ gitfolio-auth/src/main/java/com/be/gitfolio/auth/GitfolioAuthApplication.java
@@ -0,0 +1,13 @@
+package com.be.gitfolio.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitfolioAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitfolioAuthApplication.class, args);
+    }
+
+}
---
+++ gitfolio-auth/src/test/java/com/be/gitfolio/auth/GitfolioAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.gitfolio.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitfolioAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
