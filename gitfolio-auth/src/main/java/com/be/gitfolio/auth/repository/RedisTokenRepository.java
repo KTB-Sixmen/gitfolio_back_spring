b1a57e9135db2a2c91482ca22188bfa6b4125a3e
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-10-09 17:50:08 +0900

    feat: refresh 토큰 저장소인 RedisTokenRepository 생성

---
+++ gitfolio-auth/src/main/java/com/be/gitfolio/auth/repository/RedisTokenRepository.java
@@ -0,0 +1,35 @@
+package com.be.gitfolio.auth.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.concurrent.TimeUnit;
+
+@Repository
+@RequiredArgsConstructor
+public class RedisTokenRepository {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    // Refresh 토큰 저장
+    public void saveRefreshToken(String key, String refreshToken, Long expirationTimeMs) {
+        redisTemplate.opsForValue().set(key, refreshToken, expirationTimeMs, TimeUnit.MILLISECONDS);
+    }
+
+    // Refresh 토큰 조회
+    public String getRefreshToken(String key) {
+        return (String) redisTemplate.opsForValue().get(key);
+    }
+
+    // Refresh 토큰 삭제
+    public void deleteRefreshToken(String key) {
+        redisTemplate.delete(key);
+    }
+
+    // Refresh 토큰 존재 여부 확인
+    public boolean existsByRefreshToken(String key) {
+        return redisTemplate.hasKey(key);
+    }
+
+}
