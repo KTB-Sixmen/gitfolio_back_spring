fa0483b48593c8cdff16b7d85f12a5dd24fec105
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-10-10 21:16:49 +0900

    feat: DB별 BaseEntity 생성

---
+++ gitfolio-common/src/main/java/com/be/gitfolio/common/config/BaseEntityMongo.java
@@ -0,0 +1,18 @@
+package com.be.gitfolio.common.config;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document
+public abstract class BaseEntityMongo {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+}
