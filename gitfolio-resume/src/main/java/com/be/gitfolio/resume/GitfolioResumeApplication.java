70ddef96982d06dd3f3e0bd5912ecf8e11c8bf3c
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-10-21 14:49:17 +0900

    chore: MySQL 관련 설정

--- gitfolio-resume/src/main/java/com/be/gitfolio/resume/GitfolioResumeApplication.java
+++ gitfolio-resume/src/main/java/com/be/gitfolio/resume/GitfolioResumeApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 @EnableMongoAuditing
+@EnableJpaAuditing
 @ComponentScan(basePackages = {"com.be.gitfolio.resume", "com.be.gitfolio.common"})
 public class GitfolioResumeApplication {
 
