ed16e3bd82f7b2b5028e1251271191ec02eeaae8
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-10-21 14:48:37 +0900

    feat: 에러코드 추가

--- gitfolio-common/src/main/java/com/be/gitfolio/common/exception/ErrorCode.java
+++ gitfolio-common/src/main/java/com/be/gitfolio/common/exception/ErrorCode.java
@@ -8,6 +8,13 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
+
+    /**
+     * 댓글 에러
+     */
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글이 존재하지 않습니다."),
+    INVALID_MEMBER_TO_UPDATE_COMMENT(HttpStatus.FORBIDDEN, "댓글 작성자만 수정할 수 있습니다."),
+
     /**
      * 이력서 에러
      */
@@ -50,9 +57,8 @@ public enum ErrorCode {
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다"),
     NOT_FOUND(HttpStatus.NOT_FOUND, "찾을 수 없습니다"),
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 HTTP Method 요청입니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류입니다.");
-
-    ;
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류입니다."),
+    ;;
 
     private final HttpStatus status;
     private final String message;
