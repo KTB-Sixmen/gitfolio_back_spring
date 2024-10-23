73a216e37b5445bd976bf96cc69c50932bf260e7
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-10-23 13:22:09 +0900

    refactor: 토큰에 nickname까지 넣는것으로 수정

--- gitfolio-auth/src/main/java/com/be/gitfolio/auth/oauth2/CustomSuccessHandler.java
+++ gitfolio-auth/src/main/java/com/be/gitfolio/auth/oauth2/CustomSuccessHandler.java
@@ -38,6 +38,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         //OAuth2User
         CustomOAuth2User customUserDetails = (CustomOAuth2User) authentication.getPrincipal();
         String username = customUserDetails.getUsername();
+        String nickname = customUserDetails.getName();
         Long memberId = customUserDetails.getMemberId();
 
         Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
@@ -46,7 +47,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         String role = auth.getAuthority();
 
         //토큰 생성
-        String refresh = jwtUtil.createJwt("refresh", username, role, memberId,refreshTokenExpiry);
+        String refresh = jwtUtil.createJwt("refresh", username, nickname, role, memberId,refreshTokenExpiry);
 
         //Refresh 토큰 저장
         redisTokenRepository.saveRefreshToken(username, refresh, refreshTokenExpiry);
