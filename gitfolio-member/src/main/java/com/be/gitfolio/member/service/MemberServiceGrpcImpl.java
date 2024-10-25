8618ef7aafb0f2de193953ece2e51643d3311821
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-10-25 17:02:31 +0900

    feat: AI 모듈 연동 및 내부통신 수정

--- gitfolio-member/src/main/java/com/be/gitfolio/member/service/MemberServiceGrpcImpl.java
+++ gitfolio-member/src/main/java/com/be/gitfolio/member/service/MemberServiceGrpcImpl.java
@@ -1,10 +1,10 @@
 package com.be.gitfolio.member.service;
 
-import com.be.gitfolio.common.exception.BaseException;
-import com.be.gitfolio.common.exception.ErrorCode;
 import com.be.gitfolio.common.grpc.MemberServiceGrpc;
-import com.be.gitfolio.common.grpc.MemberServiceProto;
 import com.be.gitfolio.member.domain.Member;
+import com.be.gitfolio.member.domain.MemberAdditionalInfo;
+import com.be.gitfolio.member.mapper.*;
+import com.be.gitfolio.member.repository.MemberAdditionalInfoRepository;
 import com.be.gitfolio.member.repository.MemberRepository;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.lognet.springboot.grpc.GRpcService;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static com.be.gitfolio.common.grpc.MemberServiceProto.*;
 
@@ -21,19 +22,52 @@ import static com.be.gitfolio.common.grpc.MemberServiceProto.*;
 public class MemberServiceGrpcImpl extends MemberServiceGrpc.MemberServiceImplBase {
 
     private final MemberRepository memberRepository;
+    private final MemberAdditionalInfoRepository memberAdditionalInfoRepository;
 
     @Override
     public void getMemberById(MemberRequestById request, StreamObserver<MemberResponseById> responseObserver) {
         String memberId = request.getMemberId();
 
         Optional<Member> memberOpt = memberRepository.findById(Long.valueOf(memberId));
+        Optional<MemberAdditionalInfo> memberAdditionalInfoOpt = memberAdditionalInfoRepository.findByMemberId(memberId);
 
-        if (memberOpt.isPresent()) {
+        if (memberOpt.isPresent() && memberAdditionalInfoOpt.isPresent()) {
             Member member = memberOpt.get();
+            MemberAdditionalInfo memberAdditionalInfo = memberAdditionalInfoOpt.get();
 
             MemberResponseById response = MemberResponseById.newBuilder()
                     .setMemberId(String.valueOf(member.getId()))
                     .setNickname(member.getNickname())
+                    .setMemberName(member.getName())
+                    .setAvatarUrl(member.getAvatarUrl())
+                    .setPhoneNumber(member.getPhoneNumber())
+                    .setEmail(member.getEmail())
+                    .setPosition(member.getPosition())
+                    .addAllWorkExperiences(
+                            memberAdditionalInfo.getWorkExperiences().stream()
+                            .map(ProtoMapper::toProto)
+                            .collect(Collectors.toList())
+                    )
+                    .addAllEducations(
+                            memberAdditionalInfo.getEducations().stream()
+                            .map(ProtoMapper::toProto)
+                            .collect(Collectors.toList())
+                    )
+                    .addAllCertificates(
+                            memberAdditionalInfo.getCertificates().stream()
+                                    .map(ProtoMapper::toProto)
+                                    .collect(Collectors.toList())
+                    )
+                    .addAllActivities(
+                            memberAdditionalInfo.getActivities().stream()
+                                    .map(ProtoMapper::toProto)
+                                    .collect(Collectors.toList())
+                    )
+                    .addAllLinks(
+                            memberAdditionalInfo.getLinks().stream()
+                            .map(ProtoMapper::toProto)
+                            .collect(Collectors.toList())
+                    )
                     .build();
 
             responseObserver.onNext(response);
