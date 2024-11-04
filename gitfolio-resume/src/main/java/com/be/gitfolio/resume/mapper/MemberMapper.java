8eff81515ffc284cab1e579790634a368b8baaa6
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-11-04 19:58:02 +0900

    refactor: proto와 java매퍼 설정

--- gitfolio-resume/src/main/java/com/be/gitfolio/resume/mapper/MemberMapper.java
+++ gitfolio-resume/src/main/java/com/be/gitfolio/resume/mapper/MemberMapper.java
@@ -1,52 +1,18 @@
 package com.be.gitfolio.resume.mapper;
 
 import com.be.gitfolio.common.grpc.MemberServiceProto;
-import com.be.gitfolio.common.type.EmploymentStatus;
-import com.be.gitfolio.common.type.GraduationStatus;
-import com.be.gitfolio.common.type.SchoolType;
-import com.be.gitfolio.common.type.WorkType;
 import com.be.gitfolio.resume.domain.Resume.*;
+import org.mapstruct.Mapper;
 
-public class MemberMapper {
+@Mapper(componentModel = "spring")
+public interface MemberMapper {
 
-    public static Certificate toEntity(MemberServiceProto.Certificate certificateProto) {
-        return Certificate.builder()
-                .certificateName(certificateProto.getCertificateName())
-                .certificateGrade(certificateProto.getCertificateGrade())
-                .certificatedAt(certificateProto.getCertificatedAt())
-                .certificateOrganization(certificateProto.getCertificateOrganization())
-                .build();
-    }
+    Certificate toEntity(MemberServiceProto.Certificate certificateProto);
 
-    public static Education toEntity(MemberServiceProto.Education educationProto) {
-        return Education.builder()
-                .schoolType(SchoolType.fromString(educationProto.getSchoolType()))
-                .schoolName(educationProto.getSchoolName())
-                .major(educationProto.getMajor())
-                .graduationStatus(GraduationStatus.fromString(educationProto.getGraduationStatus()))
-                .startedAt(educationProto.getStartedAt())
-                .endedAt(educationProto.getEndedAt())
-                .build();
-    }
+    Education toEntity(MemberServiceProto.Education educationProto);
 
-    public static Link toEntity(MemberServiceProto.Link linkProto) {
-        return Link.builder()
-                .linkTitle(linkProto.getLinkTitle())
-                .linkUrl(linkProto.getLinkUrl())
-                .build();
-    }
+    Link toEntity(MemberServiceProto.Link linkProto);
 
-
-    public static WorkExperience toEntity(MemberServiceProto.WorkExperience workExperienceProto) {
-        return WorkExperience.builder()
-                .companyName(workExperienceProto.getCompanyName())
-                .departmentName(workExperienceProto.getDepartmentName())
-                .role(workExperienceProto.getRole())
-                .workType(WorkType.fromString(workExperienceProto.getWorkType()))
-                .employmentStatus(EmploymentStatus.fromString(workExperienceProto.getEmploymentStatus()))
-                .startedAt(workExperienceProto.getStartedAt())
-                .endedAt(workExperienceProto.getEndedAt())
-                .build();
-    }
+    WorkExperience toEntity(MemberServiceProto.WorkExperience workExperienceProto);
 
 }
