05de87d308435278873bc8ce9b4e07363b5c6421
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-11-04 19:55:57 +0900

    refactor: 각 서버에 해당하는 webclient를 빈으로 등록

--- gitfolio-common/src/main/java/com/be/gitfolio/common/config/WebClientConfig.java
+++ gitfolio-common/src/main/java/com/be/gitfolio/common/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.be.gitfolio.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,8 +8,44 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${member.server.url}")
+    private String memberServiceBaseUrl;
+
+    @Value("${ai.server.url}")
+    private String aiServiceBaseUrl;
+
+    @Value("${github.api.url}")
+    private String githubBaseUrl;
+
+    @Value("${kakao.api.url}")
+    private String kakaoBaseUrl;
+
     @Bean
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+    public WebClient memberWebClient(WebClient.Builder builder) {
+        return builder
+                .baseUrl(memberServiceBaseUrl)
+                .build();
     }
+
+    @Bean
+    public WebClient aiWebClient(WebClient.Builder builder) {
+        return builder
+                .baseUrl(aiServiceBaseUrl)
+                .build();
+    }
+
+    @Bean
+    public WebClient githubWebClient(WebClient.Builder builder) {
+        return builder
+                .baseUrl(githubBaseUrl)
+                .build();
+    }
+
+    @Bean
+    public WebClient kakaoWebClient(WebClient.Builder builder) {
+        return builder
+                .baseUrl(kakaoBaseUrl)
+                .build();
+    }
+
 }
