dd5ced331e381ae94759d6e55f9682d1767804d7
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-12-03 17:42:51 +0900

    refactor: Notification 리팩토링

---
+++ gitfolio-notification/src/main/java/com/be/gitfolio/notification/service/port/NotificationRepository.java
@@ -0,0 +1,14 @@
+package com.be.gitfolio.notification.service.port;
+
+import com.be.gitfolio.notification.domain.Notification;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface NotificationRepository {
+    Notification save(Notification notification);
+
+    Optional<Notification> findById(Long notificationId);
+
+    List<Notification> findAllByReceiverIdAndReadFalse(Long receiverId);
+}
